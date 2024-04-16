186ed7128852b0c535c02ea6e84b0dff7b8f81f5
FelixZichenHuang <166442323+FelixZichenHuang@users.noreply.github.com>
2024-04-16 13:04:17 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,10 +2,10 @@ public class Student {
     public String name;
   
     public Student() {
-      name = "Felix Huang";
+        name = "Felix Huang";
     }
   
     public String toString() {
-      return "My name is " + name;
+        return "My name is " + name;
     }
 }
