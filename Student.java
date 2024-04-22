620d0e27b66804ad3b3bb3f10e6830f83357ae78
FelixZichenHuang <166442323+FelixZichenHuang@users.noreply.github.com>
2024-04-21 21:21:30 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,11 +1,13 @@
 public class Student {
     private String name;
+    private int volunteerHours;
 
     public Student() {
         name = "Felix Huang";
+        volunteerHours = 28;
     }
 
     public String toString() {
-        return "My name is " + name;
+        return "My name is " + name + "\nMy accumulated volunteer hours are " + volunteerHours + " hours";
     }
 }
